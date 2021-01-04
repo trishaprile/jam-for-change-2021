f01732ebfc3daee867942536ec29ac2cbd96d316
trishaprile <trishaprilee@gmail.com>
2021-01-03 23:24:27 -0800

    Set up landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="landing-container">
+        <h2 className="pretitle">
+          UC Irvine ICS Student Council
+        </h2>
+        <h1 className="main-title">
+          Jam for Change 2021
+        </h1>
+      </div>
+    )
+  }
 }
 
 export default App;
