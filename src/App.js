020519ce644f4a220f7b057b5d13bd088cb5ef43
Trisha Le <trishaprilee@gmail.com>
2021-01-11 22:00:18 -0800

    Update FAQ cards

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
+import { ChevronRight } from 'react-bootstrap-icons';
 
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 
@@ -19,24 +20,26 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <a href="https://studentcouncil.ics.uci.edu/" target="_blank" rel="noreferrer"><img src={icssc_logo} alt="ICSSC logo" className="icssc-logo-header"></img></a>
-        <Container className="landing-container">
-          <h2 className="pretitle">
-            UC Irvine ICS Student Council
-          </h2>
-          <h1 className="main-title">
-            Jam for Change 2021
-          </h1>
-          <h3 className="dates">
-            Feb 15th - Feb 19th
-          </h3>
-          <img src={globe} alt="globe" className="globe-img"></img>
-          <div className="register-container">
-            <a href="https://forms.gle/VogasEdHYQsn7QYT6" target="_blank" rel="noreferrer"><Button className="btn">REGISTER NOW!</Button></a>
-          </div>
-        </Container>
-        <Container className="info-container">
-          <div className="about">
+        <div className="landing">
+          <a href="https://studentcouncil.ics.uci.edu/" target="_blank" rel="noreferrer"><img src={icssc_logo} alt="ICSSC logo" className="icssc-logo-header"></img></a>
+          <Container className="landing-container">
+            <h2 className="pretitle">
+              UC Irvine ICS Student Council
+            </h2>
+            <h1 className="main-title">
+              Jam for Change 2021
+            </h1>
+            <h3 className="dates">
+              Feb 15th - Feb 19th
+            </h3>
+            <img src={globe} alt="globe" className="globe-img"></img>
+            <div className="register-container">
+              <a href="https://forms.gle/VogasEdHYQsn7QYT6" target="_blank" rel="noreferrer"><Button className="btn">REGISTER NOW!</Button></a>
+            </div>
+          </Container>
+        </div>
+        <div className="info">
+          <Container className="about">
             <h3 className="about-title">ICS Student Council at UC Irvine invites you to our 2021 Jam for Change!</h3>
             <p className="about-text">Jam for Change is five day long development competition where teams work together to create applications that promote social good.</p>
             <div className="schedule">
@@ -67,96 +70,116 @@ class App extends Component {
               </VerticalTimeline>
               <p className="more-events">More events TBA!</p>
             </div>
-          </div>
-          <div className="faq">
+          </Container>
+          <Container className="faq">
             <h2 className="faq-title">FAQ & Rules</h2>
             <Accordion>
               <Card className="faq-card">
-                <Accordion.Toggle className="hover-primary" as={Card.Header} eventKey="0">
-                  What is Jam for Change?
+                <Accordion.Toggle className="faq-card-header" as={Card.Header} eventKey="0">
+                  <ChevronRight className="chevron-icon"/>What is Jam for Change?
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="0">
-                  <Card.Body>Jam for Change is five day long development competition where teams work together to create applications that promote social good.</Card.Body>
+                  <Card.Body className="faq-card-body">Jam for Change is five day long development competition where teams work together to create applications that promote social good.</Card.Body>
                 </Accordion.Collapse>
               </Card>
+            </Accordion>
+            <Accordion>
               <Card className="faq-card">
-                <Accordion.Toggle className="hover-green" as={Card.Header} eventKey="1">
-                  Where is Jam for Change being held?
+                <Accordion.Toggle className="faq-card-header" as={Card.Header} eventKey="1">
+                  <ChevronRight className="chevron-icon"/>Where is Jam for Change being held?
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="1">
-                  <Card.Body>Jam for Change is held entirely online this year. Correspondence and announcements throughout the duration of the event will happen primarily through the Jam for Change Discord. Registered participants will receive an invitation to Jam for Change Discord closer to the start date of Jam for Change.</Card.Body>
+                  <Card.Body className="faq-card-body">Jam for Change is held entirely online this year. Correspondence and announcements throughout the duration of the event will happen primarily through the Jam for Change Discord. Registered participants will receive an invitation to Jam for Change Discord closer to the start date of Jam for Change.</Card.Body>
                 </Accordion.Collapse>
               </Card>
+            </Accordion>
+            <Accordion>
               <Card className="faq-card">
-                <Accordion.Toggle className="hover-yellow" as={Card.Header} eventKey="2">
-                  When is Jam for Change?
+                <Accordion.Toggle className="faq-card-header" as={Card.Header} eventKey="2">
+                  <ChevronRight className="chevron-icon"/>When is Jam for Change?
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="2">
-                  <Card.Body>Jam for Change is happening between February 15th-19th.</Card.Body>
+                  <Card.Body className="faq-card-body">Jam for Change is happening between February 15th-19th.</Card.Body>
                 </Accordion.Collapse>
               </Card>
+            </Accordion>
+            <Accordion>
               <Card className="faq-card">
-                <Accordion.Toggle className="hover-orange" as={Card.Header} eventKey="3">
-                  How much does it cost?
+                <Accordion.Toggle className="faq-card-header" as={Card.Header} eventKey="3">
+                  <ChevronRight className="chevron-icon"/>How much does it cost?
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="3">
-                  <Card.Body>Nothing. Attending Jam for Change is free.</Card.Body>
+                  <Card.Body className="faq-card-body">Nothing. Attending Jam for Change is free.</Card.Body>
                 </Accordion.Collapse>
               </Card>
+            </Accordion>
+            <Accordion>
               <Card className="faq-card">
-                <Accordion.Toggle className="hover-red" as={Card.Header} eventKey="4">
-                  Who can participate?
+                <Accordion.Toggle className="faq-card-header" as={Card.Header} eventKey="4">
+                  <ChevronRight className="chevron-icon"/>Who can participate?
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="4">
-                  <Card.Body>All UC Irvine students are welcome to participate.</Card.Body>
+                  <Card.Body className="faq-card-body">All UC Irvine students are welcome to participate.</Card.Body>
                 </Accordion.Collapse>
               </Card>
+            </Accordion>
+            <Accordion>
               <Card className="faq-card">
-                <Accordion.Toggle className="hover-primary" as={Card.Header} eventKey="5">
-                  How many people can be on a team?
+                <Accordion.Toggle className="faq-card-header" as={Card.Header} eventKey="5">
+                  <ChevronRight className="chevron-icon"/>How many people can be on a team?
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="5">
-                  <Card.Body>Teams can be up to a maximum of 4 people.</Card.Body>
+                  <Card.Body className="faq-card-body">Teams can be up to a maximum of 4 people.</Card.Body>
                 </Accordion.Collapse>
               </Card>
+            </Accordion>
+            <Accordion>
               <Card className="faq-card">
-                <Accordion.Toggle className="hover-green" as={Card.Header} eventKey="6">
-                  What if I don’t have a team?
+                <Accordion.Toggle className="faq-card-header" as={Card.Header} eventKey="6">
+                  <ChevronRight className="chevron-icon"/>What if I don’t have a team?
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="6">
-                  <Card.Body>We will be assisting with team formations during the event by creating a Discord channel for team formations.</Card.Body>
+                  <Card.Body className="faq-card-body">We will be assisting with team formations during the event by creating a Discord channel for team formations.</Card.Body>
                 </Accordion.Collapse>
               </Card>
+            </Accordion>
+            <Accordion>
               <Card className="faq-card">
-                <Accordion.Toggle className="hover-yellow" as={Card.Header} eventKey="7">
-                  How do I register?
+                <Accordion.Toggle className="faq-card-header" as={Card.Header} eventKey="7">
+                  <ChevronRight className="chevron-icon"/>How do I register?
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="7">
-                  <Card.Body>You can register by filling out the following RSVP form above.</Card.Body>
+                  <Card.Body className="faq-card-body">You can register by filling out the following RSVP form above.</Card.Body>
                 </Accordion.Collapse>
               </Card>
+            </Accordion>
+            <Accordion>
               <Card className="faq-card">
-                <Accordion.Toggle className="hover-orange" as={Card.Header} eventKey="8">
-                  May I submit pre-existing projects for Jam for Change?
+                <Accordion.Toggle className="faq-card-header" as={Card.Header} eventKey="8">
+                  <ChevronRight className="chevron-icon"/>May I submit pre-existing projects for Jam for Change?
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="8">
-                  <Card.Body>Unfortunately, all design elements and code for your project must be created during the event. Pre-existing third party tools and frameworks are allowed. Coding in advance is not allowed, but planning and discussing with teammates prior to the event is allowed.</Card.Body>
+                  <Card.Body className="faq-card-body">Unfortunately, all design elements and code for your project must be created during the event. Pre-existing third party tools and frameworks are allowed. Coding in advance is not allowed, but planning and discussing with teammates prior to the event is allowed.</Card.Body>
                 </Accordion.Collapse>
               </Card>
+            </Accordion>
+            <Accordion>
               <Card className="faq-card">
-                <Accordion.Toggle className="hover-primary" as={Card.Header} eventKey="10">
-                  What if my question isn’t answered here?
+                <Accordion.Toggle className="faq-card-header" as={Card.Header} eventKey="10">
+                  <ChevronRight className="chevron-icon"/>What if my question isn’t answered here?
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="10">
-                  <Card.Body>If your question isn’t answered in the FAQ, please leave a question in the Jam for Change Discord or email us at <a href="mailto:icssc@uci.edu">icssc@uci.edu</a></Card.Body>
+                  <Card.Body className="faq-card-body">If your question isn’t answered in the FAQ, please leave a question in the Jam for Change Discord or email us at <a href="mailto:icssc@uci.edu">icssc@uci.edu</a></Card.Body>
                 </Accordion.Collapse>
               </Card>
+            </Accordion>
+            <Accordion>
               <Card className="faq-card">
-                <Accordion.Toggle className="hover-green" as={Card.Header} eventKey="11">
-                  What are the rules?
+                <Accordion.Toggle className="faq-card-header" as={Card.Header} eventKey="11">
+                  <ChevronRight className="chevron-icon"/>What are the rules?
                 </Accordion.Toggle>
                 <Accordion.Collapse eventKey="11">
-                  <Card.Body>
+                  <Card.Body className="faq-card-body">
                     <div style={{display: "flex", flexDirection: "row"}}>
                       <div style={{display: "flex", width: "50%", flexDirection: "column", paddingRight: "2rem"}}>
                         <h3>Jam for Change Rules </h3>
@@ -189,12 +212,12 @@ class App extends Component {
                 </Accordion.Collapse>
               </Card>
             </Accordion>
-          </div>
-          <footer>
-            <a href="https://studentcouncil.ics.uci.edu/" target="_blank" rel="noreferrer"><img src={icssc_logo} alt="icssc logo" className="icssc-logo-footer"></img></a>
-            <p>© 2021 ICSSC Projects Committee</p>
-          </footer>
-        </Container>
+          </Container>
+        </div>
+        <footer>
+          <a href="https://studentcouncil.ics.uci.edu/" target="_blank" rel="noreferrer"><img src={icssc_logo} alt="icssc logo" className="icssc-logo-footer"></img></a>
+          <p>© 2021 ICSSC Projects Committee</p>
+        </footer>
       </div>
     )
   }
